04b540a037385526b7b325f980ea3f34e61c1200
Dustin Holliday <dustinmholliday@gmail.com>
2024-04-10 20:09:36 -0600

    removed AWS auth

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,9 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
-
 import { Amplify } from 'aws-amplify';
-
 import amplifyconfig from './amplifyconfiguration.json';
+import "@aws-amplify/ui-vue/styles.css";
 
 Amplify.configure(amplifyconfig);
 
